ee9dbd826a60a096c4fc9086bd51bfc151bca9fd
Andressa <acarvalho0710@gmail.com>
2025-01-01 19:25:35 -0300

    chore: updated PRISMA schema

---
+++ prisma/migrations/20250101175204_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `userType` VARCHAR(191) NOT NULL DEFAULT 'produtor',
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
